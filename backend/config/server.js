27c5e35e7a69af10878702ee30853284fda70dcf
pchatard <chatard.pierre@gmail.com>
2020-11-16 12:47:35 +0100

    Strapi initialization

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f21d236e2e119915c5b7e54542d5e996'),
+    },
+  },
+});
