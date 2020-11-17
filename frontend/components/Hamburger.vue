75a0c475efe4c98216f8d816fb39a4771e6f0ec4
pchatard <chatard.pierre@gmail.com>
2020-11-17 18:37:00 +0100

    [Nuxt] Hamburger, category fetching

---
+++ frontend/components/Hamburger.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="hamburger">
+        <input
+            id="hamburger-state"
+            type="checkbox"
+            @change="$emit('hamburger')"
+        />
+        <label for="hamburger-state" class="hamburger__button">
+            <span class="hamburger__button__line"></span>
+            <span class="hamburger__button__line"></span>
+            <span class="hamburger__button__line"></span>
+        </label>
+    </div>
+</template>
