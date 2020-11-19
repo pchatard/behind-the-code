4f1b96d0f13dc51d9489b9b1e63ff1b96c573a4b
pchatard <chatard.pierre@gmail.com>
2020-11-19 11:48:17 +0100

    [Nuxt] Testing starting point

--- frontend/pages/articles/_id.spec.js
+++ frontend/pages/articles/_id.spec.js
@@ -1,20 +1,118 @@
-import { mount } from '@vue/test-utils';
+import {
+    shallowMount,
+    mount,
+    createLocalVue,
+    RouterLinkStub,
+} from '@vue/test-utils';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import SubjectLink from '@/components/ArticlePage/SubjectLink';
+import Resource from '@/components/ArticlePage/Resource';
 import Article from './_id.vue';
 
-const article = {
-    title: 'Test',
-    content: 'Testing the component',
-    subjects: [],
-    resources: [],
-};
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(VueRouter);
 
-describe('Article page', () => {
-    const wrapper = mount(Article, {
-        propsData: {
-            article,
-        },
+describe('/articles/_id.vue', () => {
+    let store, state, routes, router;
+    beforeEach(() => {
+        state = {
+            list: [
+                {
+                    id: 1,
+                    title: 'Title',
+                    subjects: [
+                        { id: 1, title: 'Sub1' },
+                        { id: 2, title: 'Sub2' },
+                    ],
+                    resources: [
+                        { id: 1, link: '' },
+                        { id: 2, link: '' },
+                        { id: 3, link: '' },
+                    ],
+                    content: 'Content',
+                },
+            ],
+        };
+        store = new Vuex.Store({
+            modules: {
+                articles: {
+                    state,
+                    namespaced: true,
+                },
+            },
+        });
+        routes = [{ params: { id: 1 }, path: '/articles/1' }];
+        router = new VueRouter({ routes });
     });
-    test('Article is rendering a main element', () => {
-        expect(wrapper.contains('main')).toBe(true);
+
+    it('renders a main on the page', () => {
+        const wrapper = shallowMount(Article, {
+            store,
+            router,
+            localVue,
+            stubs: {
+                NuxtLink: RouterLinkStub,
+            },
+            data: () => {
+                return {
+                    id: 1,
+                };
+            },
+        });
+        expect(wrapper.get('article')).toBeTruthy();
+    });
+    it('renders a title', () => {
+        const wrapper = shallowMount(Article, {
+            store,
+            router,
+            localVue,
+            stubs: {
+                NuxtLink: RouterLinkStub,
+            },
+            data: () => {
+                return {
+                    id: 1,
+                };
+            },
+        });
+        const h1 = wrapper.get('h1');
+        expect(h1.text()).toBe(state.list[0].title);
+    });
+    it('renders a list of SubjectLinks', () => {
+        const wrapper = mount(Article, {
+            store,
+            router,
+            localVue,
+            stubs: {
+                NuxtLink: RouterLinkStub,
+            },
+            data: () => {
+                return {
+                    id: 1,
+                };
+            },
+        });
+        const subjects = wrapper.findAllComponents(SubjectLink);
+        // console.log(subjects);
+        expect(subjects).toHaveLength(2);
+    });
+    it('renders a list of Resources', () => {
+        const wrapper = mount(Article, {
+            store,
+            router,
+            localVue,
+            stubs: {
+                NuxtLink: RouterLinkStub,
+            },
+            data: () => {
+                return {
+                    id: 1,
+                };
+            },
+        });
+        const resources = wrapper.findAllComponents(Resource);
+        expect(resources).toHaveLength(3);
     });
 });
