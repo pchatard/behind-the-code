f6d37b199cd6d4be8480c1827ea6c851a9f59b0a
pchatard <chatard.pierre@gmail.com>
2020-11-21 21:52:39 +0100

    [Nuxt] Footer

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,3 +1,48 @@
 <template>
-    <footer>© Pierre Chatard, 2020</footer>
+    <footer class="footer">
+        <div class="social-links">
+            <a href="/" target="_blank" rel="noopener noreferrer">
+                <img src="~assets/profile.jpg" alt="" />
+            </a>
+            <a
+                href="https://www.github.com/pchatard"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src="~assets/github.png" alt="GitHub logo" />
+            </a>
+            <a href="/" target="_blank" rel="noopener noreferrer">
+                <img src="~assets/linkedin.png" alt="LinkedIn logo" />
+            </a>
+            <a
+                href="https://twitter.com/pierre_codes"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src="~assets/twitter.png" alt="Twitter logo" />
+            </a>
+        </div>
+        <p class="copyright">Behind The Code © {{ year }}</p>
+        <p>
+            All rights reserved. Created by
+            <a
+                href="http://"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="author"
+            >
+                Pierre Chatard
+            </a>
+        </p>
+    </footer>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            year: new Date().getFullYear(),
+        };
+    },
+};
+</script>
